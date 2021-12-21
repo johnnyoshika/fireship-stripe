a1e32d1e4697b2aaf396507cb9620aff6366cc9a
Johnny Oshika <johnnyoshika@gmail.com>
2021-12-20 21:51:23 -0800

    Configure express

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,3 +6,9 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(process.env.STRIPE_SECRET, {
   apiVersion: '2020-08-27',
 });
+
+import { app } from './api';
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+  console.log(`API available on http://localhost:${port}`);
+});
