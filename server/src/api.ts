eb18271720bef7cb2526c508eaf7f1d42132ede4
Johnny Oshika <johnnyoshika@gmail.com>
2021-12-22 22:34:29 -0800

    Add checkouts endpoint

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,6 +1,20 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { createStripeCheckoutSession } from './checkout';
 export const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: true }));
+
+function runAsync(callback: Function) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    callback(req, res, next).catch(next);
+  };
+}
+
+app.post(
+  '/checkouts',
+  runAsync(async ({ body }: Request, res: Response) => {
+    res.send(await createStripeCheckoutSession(body.line_items));
+  }),
+);
